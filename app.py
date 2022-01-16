2490e8847afeb8fbdb48ea949ae09251935dedd2
Kiara <michelle_vong@brown.edu>
2022-01-16 04:26:40 -1000

    deploying

---
+++ app.py
@@ -0,0 +1,25 @@
+"""
+Entrance of the program.
+"""
+from game import *
+import random
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+
+
+def main():
+    random.seed(RANDOM_SEED)
+    game = Game()
+    while game.running and game.current_generation < N_GEN:
+        game.reset()
+        game.run()
+        
+
+if __name__ == '__main__':
+    # main()
+    app.run(host="127.0.0.1", port=8080, debug=True)
