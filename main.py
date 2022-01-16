978ff58ab9649c0067888f419228e03b54eca0f8
Kiara <michelle_vong@brown.edu>
2022-01-16 04:48:58 -1000

    deploying

--- main.py
+++ main.py
@@ -21,5 +21,5 @@ def main():
         
 
 if __name__ == '__main__':
-    main()
-    # app.run(host="127.0.0.1", port=8080, debug=True)
+    # main()
+    app.run(host="127.0.0.1", port=8080, debug=True)
