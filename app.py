fda41db199206e2d09613f6597a6249f41ba1e75
Kiara <michelle_vong@brown.edu>
2022-01-16 18:53:45 -1000

    deploying

--- app.py
+++ app.py
@@ -18,15 +18,6 @@ def main():
         game.reset()
         game.run()
 
-@app.errorhandler(500)
-def internal_error(error):
-
-    return "500 error"
-
-@app.errorhandler(404)
-def not_found(error):
-    return "404 error",404
-
 if __name__ == '__main__':
     # main()
     # app.run(host="127.0.0.1", port=5000, debug=True, threaded=True)
